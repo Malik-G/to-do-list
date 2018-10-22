2700b5f84fa46766227d29c5ad2fd1ff4166dd7a
Malik-G <Malik-G@users.noreply.github.com>
2018-10-22 00:19:27 -0500

    changed sqlText on server GET route + edited sql db file

--- server/database.sql
+++ server/database.sql
@@ -5,3 +5,13 @@ CREATE TABLE list (
 	task varchar(300) NOT NULL,
 	completed varchar(15) NOT NULL
 );
+
+INSERT INTO list (task, completed)
+VALUES ('Cook breakfast', 'Yes');
+
+INSERT INTO list (task, completed)
+VALUES ('Morning stretching', 'Yes');
+
+INSERT INTO list (task, completed)
+VALUES ('Meal prep for the week', 'No');
+
